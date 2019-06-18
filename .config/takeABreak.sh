dab1a5e2263d352b6a0808abfe846661d94da5f8
LasseKrarup <latze95@gmail.com>
2019-06-18 22:54:02 +0200

    Made break script run on startup instead of cron job. Added sound. White logo.

--- .config/takeABreak.sh
+++ .config/takeABreak.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-ICON="/usr/share/icons/Material-Design-Icons/coffee-outline.svg"
+ICON="/usr/share/icons/Material-Design-Icons/coffee-white.svg"
 
-/usr/bin/sudo -u lasse DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/notify-send -i "$ICON" -t 5000 "Take a break!" "You've been working for 45minutes, take a short break."
+/usr/bin/sudo -u lasse DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/notify-send -i "$ICON" -t 5000 "Ready to work?" "Get your ass to it. You'll get a break in 45 minutes."
+
+while true
+    do
+        sleep 45m
+	/usr/bin/paplay /usr/share/sounds/freedesktop/stereo/bell.oga
+        /usr/bin/sudo -u lasse DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/notify-send -i "$ICON" -t 5000 "Take a break!" "You've been working for 45minutes, take a short break."
+done
