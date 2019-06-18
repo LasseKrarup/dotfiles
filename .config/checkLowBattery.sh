b9d5da5d4320e865ce683c7048adae094d7d8ed3
LasseKrarup <latze95@gmail.com>
2019-06-18 09:48:51 +0200

    Fixed low battery script

--- .config/checkLowBattery.sh
+++ .config/checkLowBattery.sh
@@ -5,14 +5,12 @@ TOO_LOW=20 # how low is too low?
 NOT_CHARGING="0"
 ICON="/usr/share/icons/Papirus/22x22/panel/battery-low.svg"
 
-# export DISPLAY=:0
-
 BATTERY_LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 STATUS=$(cat $POWERSUPPLY)
 
 if [ $BATTERY_LEVEL -le $TOO_LOW -a $STATUS = $NOT_CHARGING ]
 then
-    /usr/bin/notify-send -u critical -i "$ICON" -t 5000 "Battery low" "Battery level is ${BATTERY_LEVEL}%!"
+    /usr/bin/sudo -u lasse DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/notify-send -u critical -i "$ICON" -t 5000 "Battery low" "Battery level is ${BATTERY_LEVEL}%!"
 fi
 
 exit 0
